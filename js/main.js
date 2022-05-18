1d4b2b4380755a9fee33a932eceaec00745771ff
Erwann N <neveuxerwann@gmail.com>
2022-05-18 10:25:57 +0200

    counter added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 let randomNumber = Math.round(Math.random() * 500);
 let victory = false;
+let count = 0;
 
 while(!victory){
 let userInput = Number(prompt("Saisisez un nombre !"));
+count++;
 
 switch(true){
 
     case userInput === randomNumber:
-    alert('You won!');
+    alert(`You won in ${count} tries!`);
     victory = true;
     break;
 
